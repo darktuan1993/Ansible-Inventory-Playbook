89cf777ecebd3af449760e6241650adfa7b2ead1
OFFICEVNPAY\tuanbd <bachdangtuan.dev@gmail.com>
2024-01-09 10:55:53 +0700

    update

---
+++ migration_script.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#db info
+db_user="huynn"
+db_password="Huynn@9890#!"
+db_name="test_mysql_db"
+
+# path
+dump_file="/home/isofh/migration.sql"
+
+# migrate
+mysql -u $db_user -p $db_password $db_name < $dump_file
+
+#check
+if [ $? -eq 0 ]; then
+  echo "Migration successful."
+else
+  echo "Migration failed."
+fi
